ae146cadc6d0a00caf9daca6af42efbf92af813c
vs <valeriisklyarov@gmail.com>
2024-01-21 23:20:33 +0200

    add factories and seeds install tailwind

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+//            'user_id' => function(array $attributes) {
+//                return User::factory()->create()->id;
+//            },
+            'user_id' => User::factory(),
+            'category_id' =>Category::factory(),
+            'slug' => $this->faker->slug,
+            'title' => $this->faker->sentence,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+        ];
+    }
+}
